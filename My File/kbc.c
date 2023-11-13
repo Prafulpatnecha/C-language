571863f929b2ed5db5cb3ccf36d73eab8748ec08
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-11-13 10:55:46 +0530

    Add files via upload

---
+++ My File/kbc.c
@@ -0,0 +1,137 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a,b,c,d,e,f,g,h,i;
+	printf("Enter To Answer (A,B,C,D)\n\n");
+	printf("1) Who is the father of C language?\n A] Steve Jobs\t B] James Gosling\n C] Dennis Ritchie\t D] Rasmus Lerdorf\n :");
+	scanf(" %c",&a);
+	switch(a)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("2) Which of the following is not a valid C variable name?\n A] int number;\t B] float rate;\n C] int variable_count;\t D] int $main;\n :");
+	scanf(" %c",&b);
+	switch(b)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("Parfact Answer\n\n");
+		break;
+	}
+	printf("3) All keywords in C are in ____________\n A] LowerCase letters\t B] UpperCase letters\n C] CamelCase letters\t D] None of the mentioned\n :");
+	scanf(" %c",&c);
+	switch(c)
+	{
+		case 'A': printf("Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("4) Which of the following is true for variable names in C?\n A] They can contain alphanumeric characters as well as special characters\t B] It is not an error to declare a variable to be one of the keywords(like goto, static)\n C] Variable names cannot start with a digit\t D] Variable can be of any length\n :");
+	scanf(" %c",&d);
+	switch(d)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("5) Which is valid C expression?\n A] int my_num = 100,000;\t B] int my_num = 100000;\n C] int my num = 1000;\t D] int $my_num = 10000;\n :");
+	scanf(" %c",&e);
+	switch(e)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("6) Which of the following cannot be a variable name in C?\n A] volatile\t B] true\n C] friend\t D] export\n :");
+	scanf(" %c",&f);
+	switch(f)
+	{
+		case 'A': printf("Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("7) What is short int in C programming?\n A] The basic data type of C\t B] Qualifier\n C] Short is the qualifier and int is the basic data type\t D] All of the mentioned\n :");
+	scanf(" %c",&g);
+	switch(g)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("8) Which of the following declaration is not supported by C language?\n A] String str;\t B] char *str;\n C] float str = 3e2;\t D] Both “String str;” and “float str = 3e2;”\n :");
+	scanf(" %c",&g);
+	switch(g)
+	{
+		case 'A': printf("Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("9) Which keyword is used to prevent any changes in the variable within a C program?\n A] immutable\t B] mutable\n C] const\t D] volatile\n :");
+	scanf(" %c",&h);
+	switch(h)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'C': printf("Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+	printf("10) What is the result of logical or relational expression in C?\n A] True or False\t B] 0 or 1\n C] 0 if an expression is false and any positive number if an expression is true\n D] None of the mentioned\n :");
+	scanf(" %c",&i);
+	switch(i)
+	{
+		case 'A': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'B': printf("Parfact Answer\n\n");
+		break;
+		case 'C': printf("DOES NOT Parfact Answer\n\n");
+		break;
+		case 'D': printf("DOES NOT Parfact Answer\n\n");
+		break;
+	}
+}
