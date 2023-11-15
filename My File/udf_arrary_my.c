c75a37cafb0bc43c08a771cbd6c8bd8f0b59e9ad
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-11-15 07:18:30 +0530

    Add files via upload

---
+++ My File/udf_arrary_my.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void arr()
+{
+	char a[40]={"I Love My Mother & Father"};
+	int b=0;
+	for(b=0;b<40;b++)
+	{
+		printf("%c",a[b]);
+	}
+}
+
+void main()
+{
+	arr();
+}
