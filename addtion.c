eda8e0558cbf5cfbee91e80e2d3ddd79e998e070
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-11-20 21:52:22 +0530

    Add files via upload

---
+++ addtion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a;
+	int b;
+	int value;
+	printf("Enter The add a  value :");
+	scanf("%d%d",&a,&b);
+	value=a+b;
+	printf("Value is: %d",value);
+}
