389df60ef76553074fd0698ba86ce9a86e63b16f
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-12-07 00:25:32 +0530

    Add files via upload

---
+++ 1DArray part-2/minimum_1D_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i,a[4];
+	for(i=0; i<4; i++)
+	{
+		printf("Enter The Value A[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	if(a[0]<a[1] && a[0]<a[2] && a[0]<a[3])
+	{
+		printf("A[0] is minimum");
+	}
+	else if(a[1]<a[0] && a[1]<a[2] && a[1]<a[3])
+	{
+		printf("A[1] is minimum");
+	}
+	else if(a[2]<a[1] && a[2]<a[0] && a[2]<a[3])
+	{
+		printf("A[2] is minimum");
+	}
+	else if(a[3]<a[1] && a[3]<a[2] && a[3]<a[0])
+	{
+		printf("A[3] is minimum");
+	}
+}
