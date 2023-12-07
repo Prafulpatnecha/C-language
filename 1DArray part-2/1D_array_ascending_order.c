79dee0414c96f6b90ae50eab964a4eeef00329e7
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-12-07 09:56:21 +0530

    Add files via upload

---
+++ 1DArray part-2/1D_array_ascending_order.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,s=0;
+	printf("Enter The Size Of array : ");
+	scanf("%d",&n);
+	int i,j,a[n+10],sum;
+	for(i=0; i<n; i++)
+	{
+		printf("Enter The Value Of Array[%d] : ",i);
+		scanf("%d",&a[i]);
+		sum= sum + a[i];
+	}
+	int num= n+10+sum;
+	
+	for(i=0; i<num; i++)
+	{
+		for(j=0; j<n; j++)
+		{
+			if(a[j]==s)
+			{
+				printf("%d ",s);
+			}
+		}s++;
+	}
+}
