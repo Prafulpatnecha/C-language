dfde8aeabc5d80fa072c8e6e7bcecede67a7231b
Praful Patnecha <144161200+Prafulpatnecha@users.noreply.github.com>
2023-12-29 12:01:52 +0530

    Add files via upload

---
+++ My File/NONAME00.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{       
+	int a,b,c;
+	printf("\nEnter the Value Of Addition A : ");
+	scanf("%d",&a);
+	printf("Enter the Value Of Addition B : ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("%d",c);
+
+}
